e080ed72b1dd37a61959a6c0afaf8ead2ab88fd4
Javier Ramirez <jramir11@bancoestado.cl>
2022-10-18 18:19:16 -0300

    commit inicial

---
+++ megusta.js
@@ -0,0 +1,8 @@
+
+
+function btnLikeJS() {
+    var nump = document.getElementById("plikes");
+   /* nump.querySelector(".numLikes").innerHTML = "4";*/
+   nump.querySelector(".numLikes").innerHTML++;
+
+}

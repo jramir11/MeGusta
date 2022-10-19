09536f244ecf8aca3e010c495d1c265344ec92fe
Javier Ramirez <jramir11@bancoestado.cl>
2022-10-18 22:06:14 -0300

    commit inicial

--- megusta.js
+++ megusta.js
@@ -1,8 +1,14 @@
-
-
-function btnLikeJS() {
-    var nump = document.getElementById("plikes");
-   /* nump.querySelector(".numLikes").innerHTML = "4";*/
-   nump.querySelector(".numLikes").innerHTML++;
-
+function btnDer1() {
+   var nump = document.getElementById("plikesDer1");
+   nump.querySelector(".numLikesDer1").innerHTML++;
 }
+
+function btnDer2() {
+    var nump = document.getElementById("plikesDer2");
+    nump.querySelector(".numLikesDer2").innerHTML++;
+ }
+
+ function btnDer3() {
+    var nump = document.getElementById("plikesDer3");
+    nump.querySelector(".numLikesDer3").innerHTML++;
+ }
